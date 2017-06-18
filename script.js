8a44d9341cd0f308014c28f5757e0949fada28ba
JcRemus <remus.urda@emanuel.ro>
2017-06-18 22:14:11 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function swap (litere, index1, index2) {
+  var temp = litere[index1];
+  litere[index1] = litere[index2];
+  litere[index2] = temp;
+  return litere;
+}
+function permutare (litere, startIndex, sfIndex) {
+  if (startIndex === sfIndex) {
+	console.log(litere.join(''));
+  } else {
+    var i;
+    for (i = startIndex; i <= sfIndex; i++) {
+      swap(litere, startIndex, i);
+      permutare(litere, startIndex + 1, sfIndex);
+      swap(litere, i, startIndex); // backtrack
+    }
+  }
+}
+
+var litere = ['A','B','C'];
+permutare(litere, 0, litere.length-1); // ABC, ACB, BAC, BCA, CBA, CAB
